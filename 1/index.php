2601b05b781a41cfa75e61f158cf72594faff4e5
Nic <horrornic@gmail.com>
2013-01-30 20:47:48 +0300

    First commit
    F

---
+++ 1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<title>Главная</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<div id="wrapper">
+    
+    		<div id="header">
+            ghd;lsfhg;sd
+           	</div> <!--- Header--->
+                <div id="middle">
+               	</div> <!--- Midlle--->
+    </div> <!--- Wrapper--->
+    
+                    <div id="footer">
+                    </div> <!---Footer--->
+                    
+                    
+            
+            
+</body>
+</html>
